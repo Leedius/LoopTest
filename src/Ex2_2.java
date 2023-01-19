9b4cb23440fb2d49859069645ddb7c28a288192f
Leedius <tiuy2750@gmail.com>
2023-01-19 17:04:41 +0900

    Initial commit

---
+++ src/Ex2_2.java
@@ -0,0 +1,9 @@
+public class Ex2_1 {
+    public static void main(String[] args) {
+        int i = 0;
+        while ((i<100)){
+            i+=2;
+        }
+        System.out.println(i);
+    }
+}
